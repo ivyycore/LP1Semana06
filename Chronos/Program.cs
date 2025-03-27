56bdb6246498396f1e0f5872c6d2df6fa1d5c66d
ivyycore <mariasequeira.dev@gmail.com>
2025-03-27 18:07:44 +0000

    Made the timers

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -9,6 +9,10 @@ namespace Chronos
             Stopwatch chrono1 = new Stopwatch();
             Stopwatch chrono2 = new Stopwatch();
             
+            chrono1.Start();
+            Thread.Sleep(500);
+            chrono2.Start();
+            Thread.Sleep(250);
         }
     }
 }
