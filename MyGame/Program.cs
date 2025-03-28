264b333f03346c6c0c2937a2798cbd77decfdaba
ivyycore <mariasequeira.dev@gmail.com>
2025-03-28 13:15:44 +0000

    Fixed code

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -4,12 +4,13 @@ namespace MyGame
 {
     public class Program
     {
+        public class Enemy
+        {
+                
+        }
         private static void Main(string[] args)
         {
-            public class Enemy
-            {
-                
-            }
+
         }
     }
 }
