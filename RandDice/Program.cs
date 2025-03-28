92f95e6c1f90e19dc213db395b907bd531e65429
ivyycore <mariasequeira.dev@gmail.com>
2025-03-28 12:57:23 +0000

    Added values

---
+++ RandDice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int total = 0;
+            Random value = new Random(int.Parse(args[1]));
+        }
+    }
+}
