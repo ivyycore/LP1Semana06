06ae77352c9acb49a7ae3a384b39f116148c5eb6
ivyycore <mariasequeira.dev@gmail.com>
2025-03-28 12:58:58 +0000

    Fixed values and made random function

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -8,6 +8,10 @@ namespace RandDice
         {
             int total = 0;
             Random value = new Random(int.Parse(args[1]));
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {
+                total += value.Next(1,7);
+            }
         }
     }
 }
