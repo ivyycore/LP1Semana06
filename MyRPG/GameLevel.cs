2be59a761abad454b158ae1b0e72a8784b5ac17f
ivyycore <mariasequeira.dev@gmail.com>
2025-03-28 14:55:10 +0000

    Did SetEnemyInRooms

--- MyRPG/GameLevel.cs
+++ MyRPG/GameLevel.cs
@@ -1,12 +1,24 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Humanizer;
 
 namespace MyRPG
 {
     public class GameLevel
     {
+        private Hardness dif;
+        public Hardness GetHardness();
+        private int rooms;
+        public int GetNumRooms();
+        private string enemy;
+        public int GetEnemy();
+
+        public GameLevel(int rooms, Hardness dif)
+        {
+            this.dif = dif;
+            this.rooms = rooms;
+            this.enemy = enemy;
+        }
         
+        public void SetEnemyInRoom(int rooms, string enemy)
     }
 }
