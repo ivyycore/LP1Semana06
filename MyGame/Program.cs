723dc7d684254ef5800833036123b75ff403ab89
ivyycore <mariasequeira.dev@gmail.com>
2025-03-28 15:11:34 +0000

    Fixed Program

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -1,12 +1,26 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyGame
 {
     public class Program
     {
-        
+        private static void Main(string[] args)
+        {
+            int numberEnemies = int.Parse(args[0]);
+            Enemy[] enemies = new Enemy[numberEnemies];
+            for (int i = 0; i < numberEnemies; i++)
+            {
+                Console.Write($"Nome do inimigo {i+1}: ");
+                enemies[i] = new Enemy(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < numberEnemies; i++)
+            {
+                Console.WriteLine($"{enemies[i].GetName()} {enemies[i].GetHealth()} {enemies[i].GetShield()}");
+            }
+
+            Console.WriteLine(Enemy.HowManyPowerUps());
+        }
     }
 }
