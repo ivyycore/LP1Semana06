7dc37803320798f019078e7ecadd6b7dfec6a7ce
ivyycore <mariasequeira.dev@gmail.com>
2025-03-28 12:45:17 +0000

    Fixed RandomTest

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -7,7 +7,7 @@ namespace RandomTest
         private static void Main(string[] args)
         {
             Random r1 = new Random(22);
-            Random r2 = new Random(22);
+            Random r2 = new Random(20);
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r1.Next());
             Console.WriteLine();
