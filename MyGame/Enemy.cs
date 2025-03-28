66b343ba446a6500ac3221ee03e48e2d1482c6b2
ivyycore <mariasequeira.dev@gmail.com>
2025-03-28 15:07:09 +0000

    Finished Enemy

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -4,19 +4,48 @@ namespace MyGame
 {
     public class Enemy
     {
-        private static void Main(string[] args)
-        {
-
-        }
         private string name;
         private float health;
         private float shield;
 
+
         public Enemy(string name)
-        {   
+        {
             this.name = name;
             health = 100;
             shield = 0;
         }
+
+        public string GetName()
+        {
+            return name;
+        }
+        
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
+        public float GetShield()
+        {
+            return shield;
+        }
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public void SetName(string newName)
+        {
+            if (newName.Length > 8) name = newName.Substring(0,8);
+            else name = newName;
+        } 
     }
 }
